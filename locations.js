6d561880ec0c05dec86e289c05548a432cf93076
Luka Vasko <egponent@gmail.com>
2026-04-27 13:31:50 -0400

    Added Frauenkirche.

--- locations.js
+++ locations.js
@@ -13,7 +13,14 @@ const locations = [
     {
         name: "Frauenkirche",
         coords: [51.0519, 13.7415],
-        date: "Feb 15, 1945",
-        info: "Survived the bombs but collapsed from heat..."
+        date: "Feb 13, 1945",
+        info: "\"Church of Our Lady\" was constructed between 1726 and 1743 by George Bähr. " +
+        "After the air raid, the building stood for one day. It toppled finally since the sandstone " +
+        "foundation could not withstand the high temperatures. After Germany's reunification, it was rebuilt " +
+        "in the 1990s.",
+        imageUrl: "images/Frauenkirche.jpg",
+        sources: [
+            { label: "City of Dresden Portal", url: "https://www.dresden.de/de/tourismus/sehen/sehenswuerdigkeiten/altstadt/frauenkirche.php#?searchkey=Frauenkirche"}
+        ]
     }
 ];
